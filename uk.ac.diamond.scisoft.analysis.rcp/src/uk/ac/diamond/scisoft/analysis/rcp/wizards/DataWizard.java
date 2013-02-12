e6a7531f3cfafc4f4797c54b571d429c99af803d
matthew.gerring <matthew.gerring@gmail.com>
2013-02-12 13:51:28 +0000

    Run job in wizard progressible to make easier to cancel and to make it clear that Dawn is not locking.

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/wizards/DataWizard.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/wizards/DataWizard.java
@@ -17,16 +17,28 @@
 package uk.ac.diamond.scisoft.analysis.rcp.wizards;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Vector;
 
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWizard;
@@ -35,7 +47,7 @@ import org.slf4j.LoggerFactory;
 
 import uk.ac.diamond.scisoft.analysis.rcp.AnalysisRCPActivator;
 import uk.ac.diamond.scisoft.analysis.rcp.GDADataNature;
-import uk.ac.gda.ui.utils.ProjectUtils;
+import uk.ac.gda.common.rcp.CommonRCPActivator;
 
 /**
  * This is a sample new wizard. Its role is to create a new file 
@@ -103,29 +115,28 @@ public class DataWizard extends Wizard implements INewWizard {
 	@Override
 	public boolean performFinish() {
 	
-		final String project = page.getProject();
+		final String project   = page.getProject();
 		final String directory = page.getDirectory();
-		final String folder = page.getFolder();
+		final String folder    = page.getFolder();
 
-		final Job loadDataProject = new Job("Load data project") {
-
-			@Override
-			protected IStatus run(IProgressMonitor monitor) {
-				monitor.beginTask("Importing data", 100);
-				try {
-					ProjectUtils.createImportProjectAndFolder(project, folder, directory, GDADataNature.ID, null, monitor);
-				} catch (CoreException e) {
-					logger.error("Error creating project " + project, e);
-					return new Status(IStatus.ERROR, AnalysisRCPActivator.PLUGIN_ID, "Error creating project " + project);
+		try { // Runs job in wizard
+			getContainer().run(true, true, new IRunnableWithProgress() {
+	
+				 @Override
+				 public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+					monitor.beginTask("Importing data", 100);
+					try {
+						createProject(project, folder, directory, GDADataNature.ID, monitor);
+					} catch (CoreException e) {
+						throw new InvocationTargetException(e);
+					}
 				}
-				return new Status(IStatus.OK, AnalysisRCPActivator.PLUGIN_ID, "Project " + project + " created");
-			}
-		};
-
-		loadDataProject.setUser(true);
-		loadDataProject.setPriority(Job.DECORATE);
-		loadDataProject.schedule(100);
-		
+			});
+		} catch (Exception ne) {
+			logger.error("Error creating project " + project, ne);
+			String message = "Cannot create project because an internal error occurred.";
+			ErrorDialog.openError(Display.getDefault().getActiveShell(), "Project creation failure", message, new Status(IStatus.WARNING, "org.dawnsci.plotting", ne.getMessage(), ne));
+		}
 
 		IDialogSettings settings = getDialogSettings();
 		if( settings != null){
@@ -135,6 +146,87 @@ public class DataWizard extends Wizard implements INewWizard {
 		}
 		return true;
 	}
+	
+	/**
+	 * Designed to be run from a wizard job.
+	 * @param projectName
+	 * @param folderName
+	 * @param importFolder
+	 * @param natureId
+	 * @param monitor
+	 * @throws CoreException
+	 */
+	public static void createProject(final String     projectName, 
+									final String     folderName,
+									final String     importFolder, 
+									final String     natureId, 
+									IProgressMonitor monitor) throws CoreException {
+
+		File file = new File(importFolder);
+		final String finalFolder;
+		if (!file.exists()) {
+			finalFolder = importFolder.trim();
+			file = new File(finalFolder);
+			if (!file.exists())
+				throw new CoreException(new Status(IStatus.ERROR, CommonRCPActivator.PLUGIN_ID, 
+					"Unable to create project folder " + projectName + "." + folderName + " as folder " + finalFolder + " does not exist "));
+		} else {
+			finalFolder = importFolder;
+		}
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+
+		IProject project = root.getProject(projectName);
+		if (!project.exists()) {
+			monitor.subTask("Creating project :" + projectName);
+			project.create(monitor);
+			if (natureId != null) {
+				project.open(monitor);
+				IProjectDescription description = project.getDescription();
+				description.setNatureIds(new String[] { natureId });
+				project.setDescription(description, monitor);
+			}
+		}
+
+		project.open(monitor);
+		if (project.findMember(folderName) == null) {
+			final IFolder src = project.getFolder(folderName);
+			src.createLink(new Path(finalFolder), IResource.BACKGROUND_REFRESH, monitor);
+
+		}
+		project = root.getProject(projectName);
+		project.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+	}
+
+	public static void addRemoveNature(IProject project, IProgressMonitor monitor, boolean add, String natureId) throws CoreException{
+		IProjectDescription description = project.getDescription();
+		boolean hasNature = project.hasNature(natureId);
+		String [] newNatures=null;
+		if( add ){
+			if( !hasNature){
+				String[] natures = description.getNatureIds();
+				newNatures = new String[natures.length + 1];
+				System.arraycopy(natures, 0, newNatures, 0, natures.length);
+				newNatures[natures.length] = natureId;
+			}
+		} else {
+			if( hasNature){
+				String[] natures = description.getNatureIds();
+				Vector<String> v_newNatures= new  Vector<String>();
+				for(int i=0; i< natures.length; i++){
+					if( !natures[i].equals(natureId))
+						v_newNatures.add(natures[i]);
+				}
+				newNatures = v_newNatures.toArray(new String[0]);
+			}
+		}
+		if( newNatures != null){
+			description.setNatureIds(newNatures);
+			project.setDescription(description, monitor);
+		}
+	}
+
 
 	/**
 	 * We will accept the selection in the workbench to see if
