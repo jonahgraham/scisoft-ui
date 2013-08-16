e1ebc72e9fc05ff7d28eb02aa3b089e30e2594bd
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-08-16 09:12:06 +0100

    revert effect of merge

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/actions/ShowPlotViewHandler.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/actions/ShowPlotViewHandler.java
@@ -98,8 +98,7 @@ public class ShowPlotViewHandler extends AbstractHandler {
 					guiNamesWithData = new HashSet<String>(Arrays.asList(names));
 			} catch (Exception e) {
 				// non-fatal, just means no IN_PLOT_SERVER_SUFFIX next to view name, still shouldn't happen
-				logger.debug("Failed to get list of Gui Names from Plot Server", e);
-				guiNamesWithData = Collections.emptySet();
+				logger.error("Failed to get list of Gui Names from Plot Server", e);
 			}
 			Map<String, String> values = new HashMap<String, String>();
 			for (String view : views) {
