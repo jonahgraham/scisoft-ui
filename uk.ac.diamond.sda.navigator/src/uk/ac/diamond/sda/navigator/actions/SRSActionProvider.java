682b9d3fb3b026da030d98851fbd40ff470b8fb0
matthew.gerring <matthew.gerring@gmail.com>
2013-09-23 14:33:47 +0100

    Added action for transfering any SRS data into the 'Data' view.

--- uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/actions/SRSActionProvider.java
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/actions/SRSActionProvider.java
@@ -16,6 +16,7 @@
 
 package uk.ac.diamond.sda.navigator.actions;
 
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.navigator.CommonActionProvider;
@@ -27,7 +28,8 @@ import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
 
 public class SRSActionProvider extends CommonActionProvider {
 
-	private OpenSRSAction openAction;
+	private Action openAction;
+	private Action transferAction;
 
 	/**
 	 * Construct Property Action provider.
@@ -45,7 +47,8 @@ public class SRSActionProvider extends CommonActionProvider {
 		ICommonViewerSite viewSite = aSite.getViewSite();
 		if (viewSite instanceof ICommonViewerWorkbenchSite) {
 			ICommonViewerWorkbenchSite workbenchSite = (ICommonViewerWorkbenchSite) viewSite;
-			openAction = new OpenSRSAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
+			openAction     = new OpenSRSAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
+			transferAction = new CopyTransferableDataAction(workbenchSite.getPage(), workbenchSite.getSelectionProvider());
 		}
 	}
 
@@ -58,6 +61,9 @@ public class SRSActionProvider extends CommonActionProvider {
 		/* Set up the property open action when enabled. */
 		if (openAction.isEnabled())
 			actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, openAction);
+		
+		if (transferAction.isEnabled())
+			actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, transferAction);	
 	}
 
 	/*
@@ -66,8 +72,8 @@ public class SRSActionProvider extends CommonActionProvider {
 	 */
 	@Override
 	public void fillContextMenu(IMenuManager menu) {
-		if (openAction.isEnabled())
-			menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, openAction);
+		if (openAction.isEnabled())     menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, openAction);
+		if (transferAction.isEnabled()) menu.appendToGroup(ICommonMenuConstants.GROUP_OPEN, transferAction);
 	}
 
 }
