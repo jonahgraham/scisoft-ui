8c999cc2afafe4e1ce6c3df09d73eb1373f8befc
matthew.gerring <matthew.gerring@gmail.com>
2014-09-19 10:42:50 +0100

    EPL Header

--- uk.ac.diamond.scisoft.analysis.rcp.test/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/multiview/mock/MockConfigElem.java
+++ uk.ac.diamond.scisoft.analysis.rcp.test/src/uk/ac/diamond/scisoft/analysis/rcp/plotting/multiview/mock/MockConfigElem.java
@@ -1,16 +1,10 @@
-/**
- * <copyright>
+/*
+ * Copyright (c) 2012 Diamond Light Source Ltd.
  *
- * Copyright (c) 2008-2010 BMW Car IT and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     BMW Car IT - Initial API and implementation
- *
- * </copyright>
  */
 package uk.ac.diamond.scisoft.analysis.rcp.plotting.multiview.mock;
 
--- uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileContentProvider.java
+++ uk.ac.diamond.sda.navigator/src/uk/ac/diamond/sda/navigator/views/FileContentProvider.java
@@ -1,18 +1,10 @@
 /*
- * Copyright © 2011 Diamond Light Source Ltd.
- * Contact :  ScientificSoftware@diamond.ac.uk
- * 
- * This is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 3 as published by the Free
- * Software Foundation.
- * 
- * This software is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this software. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (c) 2012 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 
 package uk.ac.diamond.sda.navigator.views;
