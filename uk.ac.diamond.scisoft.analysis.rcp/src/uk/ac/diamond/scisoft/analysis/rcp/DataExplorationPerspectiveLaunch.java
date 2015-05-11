c9f8ee71ae51406fb3437b73eb1fc9ff0d31efdf
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-11 10:57:57 +0100

    Rename IAnalyticsTracker to EventTracker

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/DataExplorationPerspectiveLaunch.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/DataExplorationPerspectiveLaunch.java
@@ -8,7 +8,7 @@
  */
 
 package uk.ac.diamond.scisoft.analysis.rcp;
-import org.eclipse.dawnsci.analysis.api.IAnalyticsTracker;
+import org.eclipse.dawnsci.analysis.api.EventTracker;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -35,9 +35,9 @@ public class DataExplorationPerspectiveLaunch implements IWorkbenchWindowActionD
 	public void run(IAction action) {
 		try {
 			PlatformUI.getWorkbench().showPerspective(DataExplorationPerspective.ID,PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-			IAnalyticsTracker tracker = AnalyticsTrackerServiceLoader.getService();
+			EventTracker tracker = AnalyticsTrackerServiceLoader.getService();
 			if (tracker != null)
-				tracker.track("DExplore_perspective_launch", true);
+				tracker.track("DExplore_perspective_launch");
 		} catch (WorkbenchException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
