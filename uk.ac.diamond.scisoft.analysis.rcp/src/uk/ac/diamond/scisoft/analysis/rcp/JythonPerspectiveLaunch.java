de9b51e20f786cb6ecc31ade0300c3a53d29b38c
Baha El Kassaby <baha.elkassaby@gmail.com>
2015-05-12 10:30:27 +0100

    Use AbstractPerspectiveLaunch in order to simplify the perspective launch

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/JythonPerspectiveLaunch.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/JythonPerspectiveLaunch.java
@@ -1,46 +1,23 @@
 /*
- * Copyright (c) 2012 Diamond Light Source Ltd.
+ * Copyright (c) 2012, 2015 Diamond Light Source Ltd.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-
 package uk.ac.diamond.scisoft.analysis.rcp;
 
-import org.eclipse.dawnsci.analysis.api.EventTracker;
+import org.dawb.common.ui.perspective.AbstractPerspectiveLaunch;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IPerspectiveDescriptor;
-import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.WorkbenchException;
 
-
-public class JythonPerspectiveLaunch implements IWorkbenchWindowActionDelegate{
+public class JythonPerspectiveLaunch extends AbstractPerspectiveLaunch {
 
 	@Override
-	public void run(IAction action) {
-		try {
-			PlatformUI.getWorkbench().showPerspective(JythonPerspective.ID, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-
-			// track perspective launch with perspective name
-			IPerspectiveRegistry reg = PlatformUI.getWorkbench().getPerspectiveRegistry();
-			IPerspectiveDescriptor per = reg.findPerspectiveWithId(JythonPerspective.ID);
-			String perspectiveName = "NA";
-			if (per!= null)
-				perspectiveName = per.getLabel();
-			EventTracker tracker = EventTrackerServiceLoader.getService();
-			if (tracker != null)
-				tracker.track(perspectiveName);
-		} catch (WorkbenchException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public String getID() {
+		return JythonPerspective.ID;
 	}
 
 	@Override
@@ -57,5 +34,4 @@ public class JythonPerspectiveLaunch implements IWorkbenchWindowActionDelegate{
 	public void init(IWorkbenchWindow window) {
 		
 	}
-
 }
