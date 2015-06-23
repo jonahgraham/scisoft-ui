8a7bde2b3832cb56cf8b532f3a0f96261e0356e7
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-06-23 13:43:47 +0100

    Preliminary work for EMPEditor, not clear yet if an editor is required.

---
+++ uk.ac.diamond.scisoft.mrc.ui/src/uk/ac/diamond/scisoft/mrc/ui/EMPEditor.java
@@ -0,0 +1,48 @@
+package uk.ac.diamond.scisoft.mrc.ui;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * An editor part for providing a human 
+ * @author fcp94556
+ *
+ */
+public class EMPEditor extends MultiPageEditorPart {
+	
+	private static Logger logger = LoggerFactory.getLogger(EMPEditor.class);
+
+	@Override
+	protected void createPages() {
+		
+		final TextEditor source = new TextEditor();
+		try {
+			addPage(0, source,       getEditorInput());
+			setPageText(0, getEditorInput().getName());
+		} catch (PartInitException e) {
+			logger.error("Creating editor!", e);
+		}
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doSaveAs() {
+		
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+
+
+}
