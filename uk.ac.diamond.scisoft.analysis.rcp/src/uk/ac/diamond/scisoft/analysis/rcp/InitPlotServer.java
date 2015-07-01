1be979278e53cc14c4a90138d677d11c1b264f4f
Simon Berriman <simon.berriman@diamond.ac.uk>
2015-07-01 16:17:24 +0100

    Amendments for RMI server to allow correct order for startup and port setting. Also consolidates sanity checking.

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/InitPlotServer.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/InitPlotServer.java
@@ -12,7 +12,6 @@ package uk.ac.diamond.scisoft.analysis.rcp;
 import org.dawb.common.util.net.NetUtils;
 import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.dawnsci.analysis.api.RMIServerProvider;
 import org.eclipse.dawnsci.analysis.api.ServerPortEvent;
 import org.eclipse.dawnsci.analysis.api.ServerPortListener;
 import org.eclipse.ui.IStartup;
@@ -28,10 +27,9 @@ import uk.ac.diamond.scisoft.analysis.rcp.preference.AnalysisRpcAndRmiPreference
 import uk.ac.diamond.scisoft.analysis.rcp.preference.PreferenceConstants;
 import uk.ac.diamond.scisoft.analysis.rpc.FlatteningService;
 
-public class InitPlotServer implements IStartup, ServerPortListener{
-	
-	private static final Logger logger = LoggerFactory.getLogger(InitPlotServer.class);
+public class InitPlotServer implements IStartup, ServerPortListener {
 
+	private static final Logger logger = LoggerFactory.getLogger(InitPlotServer.class);
 
 	@SuppressWarnings("rawtypes")
 	private ServiceTracker plotServerTracker;
@@ -40,30 +38,26 @@ public class InitPlotServer implements IStartup, ServerPortListener{
 	@Override
 	public void earlyStartup() {
 
-		if (AnalysisRCPActivator.isCorbaClient()) return;
-		
+		if (AnalysisRCPActivator.isGDA()) return;
+
 		AnalysisRpcServerProvider.getInstance().addPortListener(this);
 
 		plotServerTracker = new ServiceTracker(AnalysisRCPActivator.getDefault().getBundleContext(), PlotServer.class.getName(), null);
 		plotServerTracker.open();
 		PlotServer plotServer = (PlotServer)plotServerTracker.getService();
-		if( plotServer != null) PlotServerProvider.setPlotServer(plotServer);			
-		
-		// if the rmi server has been vetoed, dont start it up, this also has issues
-		if (Boolean.getBoolean("uk.ac.diamond.scisoft.analysis.analysisrpcserverprovider.disable") == false) {
-			
-			int analysisPort = AnalysisRpcAndRmiPreferencePage.getAnalysisRpcPort();
-			if (analysisPort<1 || NetUtils.isPortFree(analysisPort)) AnalysisRpcServerProvider.getInstance().setPort(analysisPort);
+		if( plotServer != null) PlotServerProvider.setPlotServer(plotServer);
 
-			int rmiPort = AnalysisRpcAndRmiPreferencePage.getRmiPort();
-			if (rmiPort<1 || NetUtils.isPortFree(rmiPort)) {
-				int port = RMIServerProvider.getInstance().getPort();
-				if (port<1) RMIServerProvider.getInstance().setPort(rmiPort);
-			}
-			
-			FlatteningService.getFlattener().setTempLocation(AnalysisRpcAndRmiPreferencePage.getAnalysisRpcTempFileLocation());
+
+		// If the RMI server has been vetoed, don't start it up, this also has issues
+		//if (Boolean.getBoolean("uk.ac.diamond.scisoft.analysis.analysisrpcserverprovider.disable") == false) {
+		final int analysisPort = AnalysisRpcAndRmiPreferencePage.getAnalysisRpcPort();
+		if (analysisPort >= 1 && analysisPort <= 65535 && NetUtils.isPortFree(analysisPort)) {
+			AnalysisRpcServerProvider.getInstance().setPort(analysisPort);
 		}
-		
+
+		FlatteningService.getFlattener().setTempLocation(AnalysisRpcAndRmiPreferencePage.getAnalysisRpcTempFileLocation());
+		//}
+
 		// Close the plot server when we shut down.
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 
@@ -73,7 +67,7 @@ public class InitPlotServer implements IStartup, ServerPortListener{
 					PlotServer plotServer = (PlotServer)plotServerTracker.getService();
 					if( plotServer != null)
 						PlotServerProvider.setPlotServer(null);
-					plotServerTracker.close();		
+					plotServerTracker.close();
 				} catch (Throwable ne) {
 					// Ignored we are shutting down.
 				}
@@ -82,10 +76,7 @@ public class InitPlotServer implements IStartup, ServerPortListener{
 		}));
 
 		PlotServerProvider.getPlotServer(); // Creates plot server if required.
-
-
 	}
-
 
 	@Override
 	public void portAssigned(ServerPortEvent evt) {
